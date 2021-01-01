e9077ee6a2167a69287024400ec20fa6018f41b6
Abhishek Doshi <adoshi26.ad@gmail.com>
2021-01-01 16:14:59 +0530

    added injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,38 @@
 import 'package:get_it/get_it.dart';
+import 'package:tdd_weather/core/webservice/api_client.dart';
+import 'package:tdd_weather/features/get_weather/data/datasources/weather_remote.dart';
+import 'package:tdd_weather/features/get_weather/data/datasources/weather_remote_impl.dart';
+import 'package:tdd_weather/features/get_weather/data/repositories/weather_repository_impl.dart';
+import 'package:tdd_weather/features/get_weather/domain/repositories/weather_repository.dart';
+import 'package:tdd_weather/features/get_weather/domain/usecases/get_weather_usecase.dart';
+import 'package:tdd_weather/features/get_weather/preseantation/bloc/weather_bloc.dart';
 
 final serviceLocation = GetIt.instance;
 
-Future<void> init() async {}
+Future<void> init() async {
+  serviceLocation.registerFactory(
+    () => WeatherBloc(
+      weatherUseCase: serviceLocation(),
+    ),
+  );
+
+  serviceLocation.registerLazySingleton(
+    () => WeatherUseCase(
+      repository: serviceLocation(),
+    ),
+  );
+
+  serviceLocation.registerLazySingleton<WeatherRepository>(
+    () => WeatherRepositoryImpl(
+      weatherData: serviceLocation(),
+    ),
+  );
+
+  serviceLocation.registerLazySingleton<WeatherRemote>(
+    () => WeatherRemoteImpl(
+      client: serviceLocation(),
+    ),
+  );
+
+  serviceLocation.registerLazySingleton(() => ApiClient.create());
+}
