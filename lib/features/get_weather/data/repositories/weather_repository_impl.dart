4ae569e414b75744b9283ad38fb7a7954cbd8782
Abhishek Doshi <adoshi26.ad@gmail.com>
2021-01-01 15:49:58 +0530

    weather repository implementation done

--- lib/features/get_weather/data/repositories/weather_repository_impl.dart
+++ lib/features/get_weather/data/repositories/weather_repository_impl.dart
@@ -1,11 +1,22 @@
+import 'package:tdd_weather/core/error/exception.dart';
+import 'package:tdd_weather/features/get_weather/data/datasources/weather_remote.dart';
 import 'package:tdd_weather/features/get_weather/data/models/weather_model.dart';
 import 'package:tdd_weather/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:tdd_weather/features/get_weather/domain/repositories/weather_repository.dart';
 
 class WeatherRepositoryImpl implements WeatherRepository {
+  final WeatherRemote weatherData;
+
+  WeatherRepositoryImpl({this.weatherData});
+
   @override
-  Future<Either<Failure, WeatherModel>> getWeather(String city) {
-    throw UnimplementedError();
+  Future<Either<Failure, WeatherModel>> getWeather(String city) async {
+    try {
+      final data = await weatherData.getWeatherRemote(city);
+      return Right(data);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 }
